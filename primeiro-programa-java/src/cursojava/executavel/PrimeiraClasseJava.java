4ee9ed0d277214f7637cbcc1f22c5bfd8d6c655c
zaque <zaque@DESKTOP-FN2LRVK>
2021-06-28 16:00:01 -0300

    Criacao do projeto

---
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -0,0 +1,10 @@
+package cursojava.executavel;
+
+public class PrimeiraClasseJava {
+
+	public static void main(String[] args) {
+		
+		System.out.println("MEU PRIMEIRO PROGRAMA JAVA - ZAQUEU HONORIO DE LIMA");
+		
+	}
+}
